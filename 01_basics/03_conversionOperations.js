54379411f8f9ccc2c8ddc81fbbaf478c1dc42471
SOLANKI TUSHARKUMAR RAMAJIBHAI <solankitusharramjibhai@gmail.com>
2025-08-03 18:27:01 +0000

    operations (js memes)

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -32,6 +32,35 @@ console.log(stringValue);
 console.log(typeof stringValue); //string
 
 
+// operation
+
+let value=3;
+let negValue=-value;
+// console.log(negValue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+console.log(1+"2");//12
+console.log(1+2+"2");32
+console.log("1"+2);//12
+console.log("1"+1+"2"+1); //1121
+
+console.log(+true); //1
+console.log(+""); //0
+
+//prefix and postfix ++num num++
+
+
+
+
+
+
+
 
 
 

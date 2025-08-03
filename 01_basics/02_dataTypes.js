8bdbc1b18d7f0b05d9eb73204b8572614a18c308
SOLANKI TUSHARKUMAR RAMAJIBHAI <solankitusharramjibhai@gmail.com>
2025-08-03 17:58:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); //we are using nodejs, not browser
+
+let name="tushar"
+let age=21
+let isLogged=true
+
+//number 
+//bigint
+// bigint =>""
+//null=>standalone value
+//undefined=> 
+//symbol=>unique
+
+//object=>{}
+
+console.log(typeof("tushar")); //string
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
+
+
+
+

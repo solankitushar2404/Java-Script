c6cdc58c6e01b0ba830e3f85d8bb7cb0afb762da
SOLANKI TUSHARKUMAR RAMAJIBHAI <solankitusharramjibhai@gmail.com>
2025-08-03 18:35:06 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(1>=2);
+// console.log(1>2);
+// console.log(1<=2);
+// console.log(1<2);
+// console.log(1!=2);
+// console.log(1==2);
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+console.log(undefined>0); //false
+console.log(undefined==0);//false
+console.log(undefined>=0); //false
+
+
+//null>0 undefined>0 comparision
+
+//=== strict check with data type
+
+console.log("2"===2); //false
